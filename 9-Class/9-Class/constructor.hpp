640badd78fb017ddae7f3c1f9d94f734b8def66a
wangjianlong <wangjianlong@xiaomi.com>
2018-12-22 11:12:28 +0800

    权限

--- 9-Class/9-Class/constructor.hpp
+++ 9-Class/9-Class/constructor.hpp
@@ -15,6 +15,7 @@ class Person {
     int m_age = 0;
     
 public:
+    float m_height = 0;
     Person();//error1 必须声明一个自定义的
     Person(int age);
     ~Person();
@@ -22,13 +23,35 @@ public:
     int getAge();
 };
 
-class Student : Person{
+class Student : Person
+{
+    //不写,默认是私有的.只有当前类可以访问
     float m_score;
-    
+private:
+    float m_num;
+    //受保护的,只有当前类和子类可以访问
+protected:
+    float m_class;
+    //对外公开,全部都可以访问
 public:
     
     void setScore(float score);
     float getScore();
 };
+
+class GoodStudent : public Student
+{
+public:
+    void study()
+    {
+        //'m_height' is a private member of 'Person'
+        //只有将上面的student类改成公有继承Person,才能保证子类可以访问
+//        this->m_height = 10;
+        
+    }
+    
+};
+
+//继承的时候,一般为public 继承.这样可以保持父类对成员(成员变量和成员函数)访问权限的控制...因为.继承的权限和自定义的权限,取最低值
 
 #endif /* constructor_hpp */
