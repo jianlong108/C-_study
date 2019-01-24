573ccc19e7d6b3f9ae0ad13748d5a1387a85d6db
wangjianlong <wangjianlong@xiaomi.com>
2019-01-24 16:33:38 +0800

    插入排序算法

--- 18-Copy Constructor/18-Copy Constructor/main.cpp
+++ 18-Copy Constructor/18-Copy Constructor/main.cpp
@@ -38,6 +38,30 @@ vector<int> selectSort(vector<int>sourceArray) {
     return arr;
 }
 
+//插入排序
+vector<int> insertionSorting(vector<int>sourceArray) {
+    // 对 arr 进行拷贝，不改变参数内容
+    vector<int> arr(sourceArray);
+    
+    // 从下标为1的元素开始选择合适的位置插入，因为下标为0的只有一个元素，默认是有序的
+    for (int i = 1; i < arr.size(); i++) {
+        // 记录要插入的数据
+        int tmp = arr[i];
+        // 从已经排序的序列最右边的开始比较，找到比其小的数
+        int j = i;
+        while (j > 0 && tmp < arr[j - 1]) {
+            arr[j] = arr[j - 1];
+            j--;
+        }
+
+        // 存在比其小的数，插入
+        if (j != i) {
+            arr[j] = tmp;
+        }
+    }
+    return arr;
+}
+
 int main(int argc, const char * argv[]) {
     
 //    Car c = Car();
