1778053670ffd3cad2e46f79ee5dea91b979d2a3
wangjianlong <wangjianlong@xiaomi.com>
2019-02-14 10:54:43 +0800

    两数相加算法

---
+++ 算法/算法/main.cpp
@@ -0,0 +1,59 @@
+//
+//  main.cpp
+//  算法
+//
+//  Created by wangjianlong on 2019/2/14.
+//  Copyright © 2019 wangjianlong. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+//
+//你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> v1;
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = i + 1; j < nums.size(); j++) {
+            if (nums[i] + nums[j] == target) {
+                v1.push_back(i);
+                v1.push_back(j);
+                break;
+            }
+        }
+    }
+    return v1;
+};
+
+vector<int> twoSum_hash(vector<int>& nums, int target) {
+    vector<int> v1;
+    map<int,int> tmpMap;
+    for (int i = 0; i<nums.size(); i++) {
+        tmpMap.insert(map<int, int>::value_type(nums[i],i));
+    }
+    
+    for (int i = 0; i < nums.size(); i++) {
+        int tmp = target - nums[i];
+        map<int,int>::iterator iter = tmpMap.find(tmp);
+        if (iter != tmpMap.end() && iter->second != i) {
+            v1.push_back(i);
+            v1.push_back(iter->second);
+            break;
+        }
+    }
+    return v1;
+};
+
+
+int main(int argc, const char * argv[]) {
+    vector<int>sourceArray{2,7,11,15};
+    int target = 9;
+    vector<int>v = twoSum_hash(sourceArray,target);
+    cout << v[0] << " " << v[1] << endl;
+    return 0;
+}
