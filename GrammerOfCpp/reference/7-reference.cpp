1975071ad094389fbaefeea7fe42acbb2be592c2
wangjianlong <wangjianlong@xiaomi.com>
2019-09-01 01:34:46 +0800

    reference

--- GrammerOfCpp/reference/7-reference.cpp
+++ GrammerOfCpp/reference/7-reference.cpp
@@ -24,29 +24,15 @@ struct Student {
     int age;
 };
 
-//定义全局变量
-int gA = 88;
+////定义全局变量
+//int gA = 88;
+//
+////定义一个函数 返回一个指向 int的引用
+//int & changeG()
+//{
+//    return gA;
+//}
 
-//定义一个函数 返回一个指向 int的引用
-int & changeG()
-{
-    return gA;
-}
-
-//两种交换两个数的方式
-void pSwap(int *a,int *b)
-{
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
-void rSwap(int &a,int &b)
-{
-    int tmp = a;
-    a = b;
-    b = tmp;
-}
 
 void referenceMain() {
     //lea 将地址值 存存储到寄存器。。基本在操作指针
@@ -70,17 +56,6 @@ void referenceMain() {
      
      */
     
-    int age = 10;
-    
-    //定义指针
-    int *pAge = &age;
-    
-    *pAge = 20;
-    
-    //定义引用
-    int &rAge = age;
-    rAge = 30;
-    
     //不存在引用的引用
     //int &&rrAge = rAge;
     
@@ -88,88 +63,6 @@ void referenceMain() {
     //int & *pRage = &rAge;
 //    int *p_rAge = &rAge; //相当于 int *p_rAge = &age;
     
-    
-    //为枚举定义引用
-    Season season;
-    
-    Season &rSeason = season;
-    rSeason = winter;
-    cout << season << endl;
-    
-    //为结构体定义引用
-    Student stu;
-    Student &rStu = stu;
-    rStu.age = 10;
-    
-    cout << stu.age << endl;
-    
-    //为指针定义引用
-    int height = 100;
-    int width = 200;
-    cout << "变量height 地址 = "<< &height <<endl;
-    cout << "变量width 地址 = "<< &width <<endl;
-    
-    int *a = &height;
-    int *b = &width;
-    
-    
-    cout << "指针变量 a  = "<< a <<endl;
-    cout << "指针变量 b  = "<< b <<endl;
-    
-    int *&rA = a;
-    cout << "引用变量 rA  = "<< rA <<endl;
-    //这个不是将b赋值给引用变量rA，而是将b赋值给a( a = b )
-    //此时 指针a也指向了width.
-    rA = b;
-    
-    //操作指针变量a，使其指向age变量
-    a = &age;
-    
-    cout << "引用变量 rA  = "<< rA <<endl;
-    //此时修改a指向的变量的值,相当于修改width的值
-    //    *a = 300;
-    //    cout << "height = " << height << endl;
-    //    cout << "width = " << width << endl;
-    
-    //此时相当于修改height
-    *a = 400;
-    cout << "height = " << height << endl;
-    cout << "width = " << width << endl;
-    
-    
-    //为数组定义引用
-    int array[] = {1,2,3};
-    
-    //不存在引用数组
-//    int &rArray[] = array;
-    
-    int (&rArray)[3] = array;
-    cout << "数组引用：" << rArray[1] << endl;
-    
-    //指向数组的指针
-    int *pArr = array;
-    cout << "数组指针：" << pArr[2] << endl;
-    
-    // 指针的常引用 可以表示数组
-    int * const &rArr = array;
-    cout << "指针的常引用：" << rArr[0] << endl;
-    
-    //    int *pA[4];//pA是数组,里面放着4个int *的指针
-    //    int (*pB)[4];//pA是指向数组的指针,且数组中存放的是4个int
-    
-    int w = 66;
-    int *pW = &w;
-    int *&rW = pW;
-    *rW = 88;
-    
-    
-    cout << w << endl;
-    cout << pW << endl;
-    cout << rW << endl;
-    
-    //引用的价值。。。修改函数返回值
-    changeG() = 66;
-    cout << gA << endl;
     
     int score = 100;
     const int &rScore = score;
@@ -188,13 +81,6 @@ void referenceMain() {
     (sA = sB) = 30;
     cout << "sA = " << sA << endl;
     
-    pSwap(&sA, &sB);
-    cout << sA << endl;
-    cout << sB << endl;
-    
-    rSwap(sA, sB);
-    cout << sA << endl;
-    cout << sB << endl;
     
     
     
--- GrammerOfCpp/reference/main.cpp
+++ GrammerOfCpp/reference/main.cpp
@@ -9,7 +9,175 @@
 #include <iostream>
 #include "7-reference.hpp"
 
-int main(int argc, const char * argv[]) {
-    referenceMain();
-    return 0;
+using namespace std;
+
+//定义枚举
+enum Season {
+    spring = 1,
+    summer = 2,
+    fall = 3,
+    winter = 4
+};
+
+void enumReference() {
+    Season s = spring;
+    Season &r_s = s;
+    r_s = winter;
+    cout << s <<endl;
+}
+
+//定义结构体
+struct Student {
+    int age;
+    void run(){
+        cout << "Student run" << endl;
+    }
+};
+
+void structReference() {
+    Student s = Student();
+    Student &r_s = s;
+    r_s.age = 10;
+    cout << s.age <<endl;
+}
+
+void cPointer()
+{
+    int a = 10;
+    int *p = &a;
+    *p = 20;
+    cout << a <<endl;
+}
+
+void referenceInt()
+{
+    int a = 10;
+    int &p = a;
+    p = 20;
+    cout << a <<endl;
+}
+
+void pointerReference() {
+    int a = 1;
+    int *p_a = &a;
+    int *&r_p = p_a;
+    *r_p = 2;
+    cout << a << endl;
+}
+
+void arrayReference() {
+    int arr[] = {1,2,3};
+//    int &r_arr[] = arr;
+    int (&r_arr)[3] = arr;
+    cout << "数组引用：" << r_arr[1] << endl;
+
+}
+
+void referenceCanChange() {
+    
+    int a = 10;
+    int &r_a = a;
+    int b = 11;
+    r_a = b;
+    cout << "a = " <<a <<endl;
+    cout << "b = " << b <<endl;
+    r_a = 20;
+    cout << "a = " <<a <<endl;
+    cout << "b = " << b <<endl;
+    
+}
+
+class Person {
+public:
+    void study(){
+        cout << "person study" << endl;
+    }
+};
+
+
+void notSafePointer() {
+//    Person s = Person();
+//    Person *p_a = &s;
+//    p_a->study();
+//    cout << p_a << endl;
+//    {
+//        Student b = Student();
+//        p_a = (Person *)&b;
+//
+//        Student *p_s = (Student *)p_a;
+//        p_s->run();
+//
+//        delete p_a;
+//    }
+//    cout << p_a << endl;
+//
+//    p_a->study();
+    
+    Person s = Person();
+    Person &r_s = s;
+    r_s.study();
+    
+    Student b = Student();
+    Student &r_b = b;
+    r_s = (Person &)r_b;
+    r_s.study();
+}
+
+//定义全局变量
+int gA = 88;
+
+//定义一个函数 返回一个指向 int的引用
+int & changeG()
+{
+    return gA;
+}
+
+//int main(int argc, const char * argv[]) {
+//
+////    referenceMain();
+////    referenceInt();
+////    structReference();
+////    notSafePointer();
+////    pointerReference();
+////    arrayReference();
+//    //修改函数返回值
+//    changeG() = 66;
+//    cout << gA << endl;
+//    
+//    
+//    return 0;
+//}
+
+//两种交换两个数的方式
+void pSwap(int *a,int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void rSwap(int &a,int &b)
+{
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+int main() {
+    int sA = 10;
+    int sB = 20;
+    
+    pSwap(&sA, &sB);
+    cout << "sA = " << sA << endl;
+    cout << "sB = "<< sB << endl;
+    
+    rSwap(sA, sB);
+    cout << "sA = " << sA << endl;
+    cout << "sB = "<< sB << endl;
+}
+
+void test()
+{
+    //    int a = 10;
+    //    int &r_a;
+    //    r_a = a;
 }
